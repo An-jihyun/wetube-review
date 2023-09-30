fe83c93a81f460f4dfcb7bca9191d8a5712f7eec
An-jihyun <anjh322@gmail.com>
2023-10-01 03:41:35 +0900

    Flash messages

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -30,7 +31,8 @@ app.use((req, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
-  
+
+app.use(flash());  
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
